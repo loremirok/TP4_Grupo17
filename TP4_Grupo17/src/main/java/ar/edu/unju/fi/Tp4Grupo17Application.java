c59805c510acc67432468cfa2099e91998518d90
User <User@DESKTOP-85PBKTA>
2022-04-26 14:35:22 -0300

    Iniciando tp y agregando ejercicio 1

---
+++ TP4_Grupo17/src/main/java/ar/edu/unju/fi/Tp4Grupo17Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo17Application.class, args);
+	}
+
+}
